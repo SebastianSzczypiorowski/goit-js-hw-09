d9459e688feee91b186d55411b17f07f30dcfa90
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-01-13 20:07:15 +0000

    Deploying to gh-pages from @ SebastianSzczypiorowski/goit-js-hw-09@9641ea488c8b75b85048abf9753c57e518501dcb 🚀

--- 01-color-switcher.e342cb15.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){randomHex=setInterval((function(){t.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)})),n.addEventListener("click",(function(){clearInterval(randomHex)}))}();
-//# sourceMappingURL=01-color-switcher.e342cb15.js.map
